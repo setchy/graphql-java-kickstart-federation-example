780ebf8d0555ff5aebf1bd207b907d0ebbb4a77e
Adam Setch <asetch@wfscorp.com>
2021-04-16 11:30:18 -0400

    Changing shows service to work with apollo-gateway

--- shows/src/main/java/com/example/demo/federation/FederatedSchema.java
+++ shows/src/main/java/com/example/demo/federation/FederatedSchema.java
@@ -2,12 +2,16 @@ package com.example.demo.federation;
 
 import com.apollographql.federation.graphqljava.Federation;
 import com.apollographql.federation.graphqljava._Entity;
+import com.example.demo.model.Show;
 import graphql.kickstart.tools.SchemaParser;
 import graphql.schema.GraphQLSchema;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 
@@ -15,11 +19,13 @@ import org.springframework.stereotype.Component;
 public class FederatedSchema {
 
     /**
-     * I dont believe the following is needed when using Apollo Gateway with Managed Federation.
-     * When using Apollo Gateway with `serviceList` it was complaining with the following
+     * This transformation is not necessary when using Apollo Gateway with Managed Federation.
+     * When using Apollo Gateway with an explicit `serviceList` (see apollo-gateway/index.js)
+     * it was complaining with the following"
      *
-     * Error checking for changes to service definitions: Couldn't load service definitions for "shows" at http://localhost:8080/graphql:
-     * Validation error of type FieldUndefined: Field '_service' in type 'Query' is undefined @ '_service'
+     *      Error checking for changes to service definitions: Couldn't load service definitions for
+     *      "shows" at http://localhost:8080/graphql:
+     *      Validation error of type FieldUndefined: Field '_service' in type 'Query' is undefined @ '_service'
      *
      */
 
@@ -27,22 +33,11 @@ public class FederatedSchema {
     public GraphQLSchema customSchema(SchemaParser schemaParser) {
         GraphQLSchema federatedSchema = Federation.transform(schemaParser.makeExecutableSchema())
             .fetchEntities(env -> env.<List<Map<String, Object>>>getArgument(_Entity.argumentName).stream()
-                .map(values -> {
-//                    if ("Show".equals(values.get("__typename"))) {
-//                        final Object showId = values.get("id");
-//                        if (showId instanceof String) {
-//                            return createFederatedTypeShell((String) showId);
-//                        }
-//                    }
+                .map(reference -> {
                     return null;
                 })
                 .collect(Collectors.toList()))
             .resolveEntityType(env -> {
-//                final Object src = env.getObject();
-//                if (src instanceof Show) {
-//                    return env.getSchema()
-//                        .getObjectType("Show");
-//                }
                 return null;
             })
             .build();
