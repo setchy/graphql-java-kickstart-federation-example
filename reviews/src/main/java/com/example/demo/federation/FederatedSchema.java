0d91d5f5ae54b0239d2c3dd8db9a0056ed1ff89f
Adam Setch <asetch@wfscorp.com>
2021-04-24 07:19:00 -0400

    Refactor federationTypeName into a variable

--- reviews/src/main/java/com/example/demo/federation/FederatedSchema.java
+++ reviews/src/main/java/com/example/demo/federation/FederatedSchema.java
@@ -19,11 +19,14 @@ public class FederatedSchema {
     @Bean
     public GraphQLSchema federatedGraphQLSchema(SchemaTransformer schemaTransformer)
         throws IOException {
+
+        String federationTypeName = "Show";
+
         return schemaTransformer
             .fetchEntities(env -> env.<List<Map<String, Object>>>getArgument(_Entity.argumentName)
                 .stream()
                 .map(reference -> {
-                    if ("Show".equals(reference.get("__typename"))) {
+                    if (federationTypeName.equals(reference.get("__typename"))) {
                         return ShowReferenceResolver.resolveReference(reference);
                     }
                     return null;
@@ -32,7 +35,7 @@ public class FederatedSchema {
             .resolveEntityType(env -> {
                 final Object src = env.getObject();
                 if (src instanceof Show) {
-                    return env.getSchema().getObjectType("Show");
+                    return env.getSchema().getObjectType(federationTypeName);
                 }
                 return null;
             })
